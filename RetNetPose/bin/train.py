5f02c4f962e155a57e16629354216cab6d3dc393
Stefan Thalhammer <stefan.thalhammer136@gmail.com>
2019-04-05 08:07:03 +0200

    orth loss now in name

--- RetNetPose/bin/train.py
+++ RetNetPose/bin/train.py
@@ -123,7 +123,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
         loss={
             'bbox'         : losses.smooth_l1(),
             #'3Dbox': losses.smooth_l1_xy(),
-            '3Dbox'        : losses.orthogonal_l1(),
+            '3Dbox+orth'        : losses.orthogonal_l1(),
             #'3Dbox'        : losses.weighted_mse(),
             #'3Dbox': losses.weighted_l1(),
             #'3Dbox': losses.weighted_msle(),
