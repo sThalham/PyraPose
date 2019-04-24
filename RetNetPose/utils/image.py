1459ca262499cb33ecf43922d10d158fca15eb56
Stefan Thalhammer <stefan.thalhammer136@gmail.com>
2019-04-24 18:16:24 +0200

    troubleshooting

--- RetNetPose/utils/image.py
+++ RetNetPose/utils/image.py
@@ -192,7 +192,7 @@ def resize_image(img, min_side=800, max_side=1333):
     scale = compute_resize_scale(img.shape, min_side=min_side, max_side=max_side)
 
     # resize the image with the computed scale
-    print(cv2.__version__)
+    print(cv2.__version__, scale)
     img = cv2.resize(img, None, fx=scale, fy=scale)
     print(img.shape)
 
