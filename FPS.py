8715caf6779d203c679508bea22d9c08f29dd1ff
stefan.thalhammer <stefan.thalhammer136@gmail.com>
2020-01-10 16:49:10 +0100

    farthest point sampling

---
+++ FPS.py
@@ -0,0 +1,32 @@
+import sys
+import os
+from RGBDPose.utils import ply_loader
+import numpy as np
+import transforms3d as tf3d
+import OpenEXR, Imath
+
+
+def load_pcd(cat):
+    # load meshes
+    model_vsd = ply_loader.load_ply(cat)
+
+    return model_vsd['pts']
+
+
+def main(argv):
+    root = argv[0]
+    samples = argv[1]
+    meshes = os.listdir(argv[0])
+
+    for mesh_name in meshes:
+        if mesh_name[-3:] == 'ply':
+            path = root + '/' + mesh_name
+            pts = load_pcd(path)
+
+            print(pts.shape)
+
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
