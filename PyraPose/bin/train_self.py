a2c079659a198611bec8590bc53f2dad259033ad
stefan.thalhammer <stefan.thalhammer136@gmail.com>
2021-04-07 13:43:06 +0200

    forgot to activate task

--- PyraPose/bin/train_self.py
+++ PyraPose/bin/train_self.py
@@ -105,7 +105,7 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
             '3Dbox'         : losses.orthogonal_l1(),
             'cls'           : losses.focal(),
             'mask'          : losses.focal(),
-            #'domain'        : losses.focal(),
+            'domain'        : losses.focal(),
             #'features'            : losses.smooth_l1(),
         },
         dis_loss=losses.focal()
